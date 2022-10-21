8d607f1dd4690092078c3a787a6f8faf3d53cbfa
radibit <radimir.bitsov@gmail.com>
2022-10-21 12:15:47 +0300

    Include dist directory

---
+++ dist/esm/definitions.js
@@ -0,0 +1,15 @@
+export var SampleNames;
+(function (SampleNames) {
+    SampleNames["STEP_COUNT"] = "stepCount";
+    SampleNames["FLIGHTS_CLIMBED"] = "flightsClimbed";
+    SampleNames["APPLE_EXERCISE_TIME"] = "appleExerciseTime";
+    SampleNames["ACTIVE_ENERGY_BURNED"] = "activeEnergyBurned";
+    SampleNames["BASAL_ENERGY_BURNED"] = "basalEnergyBurned";
+    SampleNames["DISTANCE_WALKING_RUNNING"] = "distanceWalkingRunning";
+    SampleNames["DISTANCE_CYCLING"] = "distanceCycling";
+    SampleNames["BLOOD_GLUCOSE"] = "bloodGlucose";
+    SampleNames["SLEEP_ANALYSIS"] = "sleepAnalysis";
+    SampleNames["WORKOUT_TYPE"] = "workoutType";
+    SampleNames["WEIGHT"] = "weight";
+})(SampleNames || (SampleNames = {}));
+//# sourceMappingURL=definitions.js.map
